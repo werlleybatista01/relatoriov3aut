427eb085ff0067eb8985fe97169f794c7ee2f121
werlleybatista01 <adeliciapereira70@gmail.com>
2026-08-03 09:19:25 -0300

    Restaura monitoramento original do almoxarifado

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createDashboardApp } from "./app.js?v=20260803-monitoramento-fix1";
+import { createDashboardApp } from "./app.js?v=20260803-monitoramento-original2";
 
 /**
  * Ponto de entrada da aplicação.
